072247b423fb03de7f076a8945fde26ab07209ba
Harold Baldwin <haroldbaldwin@hotmail.com>
2016-03-24 22:51:20 +1100

    added models

---
+++ app/src/main/java/androidcourse/notes/Models/Note.java
@@ -0,0 +1,17 @@
+package androidcourse.notes.Models;
+
+import java.util.Date;
+
+/**
+ * Created by Harold on 24/03/2016.
+ */
+public class Note {
+    private  int mId;
+    private String mTitle;
+    private String mContent;
+    private Date mLastModied;
+    private String mPassword;
+    private static int NEXt_ID = 0;
+
+}
+
