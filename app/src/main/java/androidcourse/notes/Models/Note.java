4e8b4c43cae4ebb805d6b45988526f94ba8f16a2
Harold Baldwin <haroldbaldwin@hotmail.com>
2016-03-29 00:17:52 +1100

    Added Contructor for Notes w/o a Password

--- app/src/main/java/androidcourse/notes/Models/Note.java
+++ app/src/main/java/androidcourse/notes/Models/Note.java
@@ -1,17 +1,57 @@
 package androidcourse.notes.Models;
 
+import java.text.SimpleDateFormat; // adds Simple Date Format "Package" (Package in Java, Namespace in C#)
 import java.util.Date;
 
 /**
  * Created by Harold on 24/03/2016.
  */
 public class Note {
-    private  int mId;
+    private int mId;
     private String mTitle;
     private String mContent;
     private Date mLastModied;
     private String mPassword;
-    private static int NEXt_ID = 0;
+    private static int NEXt_ID = 1;
 
+
+    public Note(String title, String content) {
+        mId = NEXt_ID++;
+        mTitle = title;
+        mContent = content;
+        mLastModied = new Date();
+    }
+
+    public Note(String titel, String content, String password) {
+        mId = NEXt_ID++;
+        mTitle = title;
+        mContent = content;
+        mPassword = password;
+        mLastModied = new Date();
+    }
+
+    public int getmId() {
+        return mId;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public String getmContent() {
+        return mContent;
+    }
+
+    public Date getmLastModied() {
+        return mLastModied;
+    }
+
+    public String getmPassword() {
+        return mPassword;
+    }
+
+    public String dateFormatted() {
+        SimpleDateFormat sdf = new SimpleDateFormat("MM dd - HH:mm");
+        return "Last edited on: " + sdf.format(mLastModied);
+    }
 }
-
