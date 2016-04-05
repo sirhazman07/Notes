41452e073297fe3aac0d7688da90103caba1eb3c
Harold Baldwin <haroldbaldwin@hotmail.com>
2016-04-05 23:55:25 +1000

    Added Intents for Activity Navigation

--- app/src/main/java/androidcourse/notes/NotesList.java
+++ app/src/main/java/androidcourse/notes/NotesList.java
@@ -1,9 +1,11 @@
 package androidcourse.notes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -13,29 +15,36 @@ import androidcourse.notes.Adapters.NotesAdapter;
 import androidcourse.notes.Models.Note;
 
 public class NotesList extends AppCompatActivity {
+    //After we set-up the on clickLIstener for the ImageView we give it an Intent an alse need to
+    // Serialize each intent (label it) through the ADD_NOTE_REQUEST which is an int initialized with 1
+    //Think of it as an Itent signature, because you may may multiple intents for the same Click event OK.
+    private ArrayList<Note> notes = new ArrayList<Note>();
+    private static final int ADD_NOTE_REQUEST = 1;
+    private NotesAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_list);
         final ListView notesListView = (ListView) findViewById(R.id.listView);
-        NotesAdapter adapter = new NotesAdapter(this, R.layout.note_row, notes);
+        adapter = new NotesAdapter(this, R.layout.note_row, notes);
         notesListView.setAdapter(adapter);
+
         notesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(NotesList.this, notes.get(position).getmTitle().toString(),
-                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        //Listener for add NoteImage
+        ImageView addNoteImg = (ImageView) findViewById(R.id.addNoteImg);
+        addNoteImg.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //create an intent
+                Intent addNoteIntent = new Intent(getBaseContext(), AddNote.class);
+                startActivityForResult(addNoteIntent,ADD_NOTE_REQUEST);
             }
         });
     }
-
-        private ArrayList<Note> notes = new ArrayList<Note>() {{
-            add(new Note("Note 1", "This is an example of my first note"));
-            add(new Note("Note 2", "This is an example of my second note"));
-            add(new Note("Note 3", "This is an example of my third note", "123"));
-            add(new Note("Note 4", "This is an example of my fourth note"));
-            add(new Note("Note 5", "This is an example of my fifth note", "123"));
-        }};
-    }
+}
 
